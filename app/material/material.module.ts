876f2e6178de8e5ade8a45cdf80ceed99abffc5e
Nasimsona <nasim.sona@gmail.com>
2019-06-30 13:52:16 -0700

    2 files modified

--- app/material/material.module.ts
+++ app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {MatTableModule} from '@angular/material/table'; 
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-  imports: [MatTableModule,MatButtonModule],
+  imports: [MatTableModule,MatButtonModule,MatMenuModule],
     
- exports: [MatTableModule,MatButtonModule] 
+ exports: [MatTableModule,MatButtonModule,MatMenuModule] 
 })
 export class MaterialModule { }
